9030879fb61ac2a23916608a1ee6b43649faf70a
John Samuel <johnsamuelwrites@gmail.com>
2022-01-21 22:45:47 +0100

    Add support for unicode punctuation and separator characters

--- inclusive/main.py
+++ inclusive/main.py
@@ -7,6 +7,9 @@
 import json
 
 import typer
+import sys
+import unicodedata
+import re
 from rich import print
 
 resource = dict()
@@ -14,11 +17,23 @@ resource["en"] = "./resources/en/list.json"
 
 app = typer.Typer()
 
+def get_all_punctuation_separator_characters():
+    characters = range(sys.maxunicode)
+    # Get all punctuation and characters, i.e., unicode category
+    # belonging to P or Z
+    # Escaping the characters ensures that these can be later used
+    # splitting text
+    punctuations_separator = set(re.escape(chr(i)) for i in characters
+                             if unicodedata.category(chr(i)).startswith("P") or
+                                unicodedata.category(chr(i)).startswith("Z"))
+    return(punctuations_separator)
+
 @app.command()
 def detect(language: str,
            filename: str = typer.Argument(None, 
                   help="File name; if missing, you will be prompted to enter a text")):
     suggestions = dict()
+    punctuations_separator = get_all_punctuation_separator_characters()
     with open(resource[language], "r") as suggestion_file:
         suggestion = json.load(suggestion_file)
         for key, value in suggestion.items():
@@ -30,14 +45,15 @@ def detect(language: str,
     else:
         with open(filename, "r") as text_file:
             text = text_file.read()
-    words = text.split(" ")
+    punctuations_separator = "|".join(punctuations_separator)
+    words = re.split("(" + punctuations_separator + ")", text)
     used_suggestions = set()
     for word in words:
-        if word in suggestions:
+        if word.lower() in suggestions:
             print("[bold green]" + word + " [/bold green]", end="")
             used_suggestions.add(word)
         else:
-            print(word + " ", end="")
+            print(word, end="")
     print()
     if (len(used_suggestions) > 0):
         print("Following are some suggested replacements:")
--- resources/README.md
+++ resources/README.md
@@ -1,3 +1,7 @@
 # Resources
 
 * [English](en/README.md)
+
+## References:
+* [Complete set of punctuation marks for Python (not just ASCII)](https://stackoverflow.com/questions/60983836/complete-set-of-punctuation-marks-for-python-not-just-ascii)
+* [Unicode Character Categories](https://www.compart.com/en/unicode/category)
