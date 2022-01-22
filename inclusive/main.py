827c408759001819c654e0cd3072315ea30bbcd9
John Samuel <johnsamuelwrites@gmail.com>
2022-01-22 16:21:10 +0100

    Supporting fr_FR, en, fr and en_US locales

--- inclusive/main.py
+++ inclusive/main.py
@@ -8,6 +8,8 @@ import json
 
 import typer
 import re
+import gettext
+import locale
 from rich import print
 from unicode_utils import get_all_punctuation_separator_characters
 from file_utils import read_file
@@ -17,10 +19,19 @@ resource["en"] = "./resources/en/list.json"
 
 app = typer.Typer()
 
+def get_default_locale_message_handler():
+    default, encoding = locale.getdefaultlocale()
+    lang = gettext.translation('inclusive', localedir='locales', languages=[default])
+    lang.install()
+    return lang.gettext
+
+
+
 @app.command()
 def detect(language: str,
            filename: str = typer.Argument(None, 
                   help="File name; if missing, you will be prompted to enter a text")):
+    _ = get_default_locale_message_handler()
     suggestions = dict()
     punctuations_separator = get_all_punctuation_separator_characters()
     with open(resource[language], "r") as suggestion_file:
@@ -29,7 +40,7 @@ def detect(language: str,
             suggestions[key] = value
     text = None
     if filename is None:
-        print("Enter [bold magenta]a text[/bold magenta]:")
+        print(_("Enter [bold magenta]a text[/bold magenta]:"))
         text = input()
     else:
         text = read_file(filename)
