0ab1cee191ab75416dd9b92da2996c2d1f807eac
John Samuel <johnsamuelwrites@gmail.com>
2022-01-29 12:36:48 +0100

    Rename project for create package

--- inclusive/__init__.py
+++
@@ -1,9 +0,0 @@
-#
-# SPDX-FileCopyrightText: 2022 John Samuel <johnsamuelwrites@gmail.com>
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-#
-
-"""`inclusive` is a **multilingual application** for **inclusive writing**. Languages evolve. Developed in Python, this application helps to write texts in an inclusive way. The application detects possibly problematic phrases and suggests some replacements."""
-
-__version__ = "0.2"
