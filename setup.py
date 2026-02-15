48447c5c6fb2346f72ccf6d92396614b6957131f
John Samuel <johnsamuelwrites@gmail.com>
2026-02-15 20:08:56 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -9,7 +9,10 @@ Setup file for inclusivewriting
 """
 
 import runpy
-import setuptools
+try:
+    import setuptools
+except ImportError:  # pragma: no cover
+    setuptools = None
 
 version_meta = runpy.run_path("./inclusivewriting/version.py")
 VERSION = version_meta["__version__"]
@@ -17,33 +20,34 @@ VERSION = version_meta["__version__"]
 with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="inclusivewriting",
-    version=VERSION,
-    author="John Samuel",
-    author_email="johnsamuelwrites@example.com",
-    description="Python multilingual application for inclusive writing",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/johnsamuelwrites/inclusive",
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    package_dir={"inclusivewriting": "inclusivewriting"},
-    package_data={
-        "inclusivewriting": [
-            "configuration.json",
-            "resources/*/*.json",
-            "locales/*",
-            "locales/*/*",
-            "locales/*/*/*",
-        ]
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License"
-        + " v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=["rich>=10.9.0", "typer>=0.3.2", "babel>=2.9.1"],
-    python_requires=">=3.7",
-)
+if setuptools is not None:
+    setuptools.setup(
+        name="inclusivewriting",
+        version=VERSION,
+        author="John Samuel",
+        author_email="johnsamuelwrites@example.com",
+        description="Python multilingual application for inclusive writing",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/johnsamuelwrites/inclusive",
+        packages=setuptools.find_packages(),
+        include_package_data=True,
+        package_dir={"inclusivewriting": "inclusivewriting"},
+        package_data={
+            "inclusivewriting": [
+                "configuration.json",
+                "resources/*/*.json",
+                "locales/*",
+                "locales/*/*",
+                "locales/*/*/*",
+            ]
+        },
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU General Public License"
+            + " v3 or later (GPLv3+)",
+            "Operating System :: OS Independent",
+        ],
+        install_requires=["rich>=10.9.0", "typer>=0.3.2", "babel>=2.9.1"],
+        python_requires=">=3.7",
+    )
